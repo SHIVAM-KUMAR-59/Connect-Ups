bbb00157c4355d62ab6ff455b0b247b40cce3205
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-10-30 01:19:05 +0530

    Startup Card Done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import SearchForm from '@/components/SearchForm'
+import StartupCard from '@/components/StartupCard'
 
 export default async function Home({
   searchParams,
@@ -7,17 +8,52 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>
 }) {
   const query = (await searchParams).query
-  return (
-    <section className="pink_container">
-      <h1 className="heading">
-        Pitch Your Startup <br />
-        Connect With Entrepreneurs
-      </h1>
-      <p className="sub-heading !max-w-3xl">
-        Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions
-      </p>
 
-      <SearchForm query={query} />
-    </section>
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: {
+        _id: 1,
+        name: 'John Doe',
+      },
+      _id: 1,
+      description: 'This is a description',
+      title: 'We Robots',
+      category: 'Robots',
+      image:
+        'https://plus.unsplash.com/premium_photo-1681562502996-bcfad45d4def?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cm9ib3RzfGVufDB8fDB8fHww',
+    },
+  ]
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup <br />
+          Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions
+        </p>
+
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search Results for "${query}"` : 'All Startups'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startups Found</p>
+          )}
+        </ul>
+      </section>
+    </>
   )
 }
