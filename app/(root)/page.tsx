5b9e89aba77881e4d06f33061829366f837354bd
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-10-29 14:17:13 +0530

    Live Data Fetching Done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import SearchForm from '@/components/SearchForm'
 import StartupCard, { StartupTypeCard } from '@/components/StartupCard'
-import { client } from '@/sanity/lib/client'
+import { sanityFetch, SanityLive } from '@/sanity/lib/live'
 import { STARTUP_QUERY } from '@/sanity/lib/queries'
 
 export default async function Home({
@@ -10,7 +10,14 @@ export default async function Home({
 }) {
   const query = (await searchParams).query
 
-  const posts = await client.fetch(STARTUP_QUERY)
+  const params = {
+    search: query || null,
+  }
+
+  const { data: posts } = await sanityFetch({
+    query: STARTUP_QUERY,
+    params,
+  })
 
   return (
     <>
@@ -40,6 +47,8 @@ export default async function Home({
           )}
         </ul>
       </section>
+
+      <SanityLive />
     </>
   )
 }
