e299fd124b43a22956279adac3dd7804b403637b
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-10-30 16:45:25 +0530

    Author Authentication Done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,8 @@
 import SearchForm from '@/components/SearchForm'
 import StartupCard, { StartupTypeCard } from '@/components/StartupCard'
-import { sanityFetch, SanityLive } from '@/sanity/lib/live'
 import { STARTUPS_QUERY } from '@/sanity/lib/queries'
+import { sanityFetch, SanityLive } from '@/sanity/lib/live'
+import { auth } from '@/auth'
 
 export default async function Home({
   searchParams,
@@ -9,9 +10,11 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>
 }) {
   const query = (await searchParams).query
-  const params = {
-    search: query || null,
-  }
+  const params = { search: query || null }
+
+  const session = await auth()
+
+  console.log(session?.id)
 
   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params })
 
@@ -19,11 +22,13 @@ export default async function Home({
     <>
       <section className="pink_container">
         <h1 className="heading">
-          Pitch Your Startup <br />
+          Pitch Your Startup, <br />
           Connect With Entrepreneurs
         </h1>
+
         <p className="sub-heading !max-w-3xl">
-          Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
         </p>
 
         <SearchForm query={query} />
@@ -31,16 +36,16 @@ export default async function Home({
 
       <section className="section_container">
         <p className="text-30-semibold">
-          {query ? `Search Results for "${query}"` : 'All Startups'}
+          {query ? `Search results for "${query}"` : 'All Startups'}
         </p>
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupTypeCard, index: number) => (
+            posts.map((post: StartupTypeCard) => (
               <StartupCard key={post?._id} post={post} />
             ))
           ) : (
-            <p className="no-results">No Startups Found</p>
+            <p className="no-results">No startups found</p>
           )}
         </ul>
       </section>
