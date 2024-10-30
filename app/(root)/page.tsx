8e707d5d88790cdbbfe2023e38bfc89a6c5bdf16
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-10-30 12:34:25 +0530

    Data Fetching Done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
-import Image from 'next/image'
 import SearchForm from '@/components/SearchForm'
-import StartupCard from '@/components/StartupCard'
+import StartupCard, { StartupTypeCard } from '@/components/StartupCard'
+import { client } from '@/sanity/lib/client'
+import { STARTUPS_QUERY } from '@/sanity/lib/queries'
 
 export default async function Home({
   searchParams,
@@ -9,22 +10,9 @@ export default async function Home({
 }) {
   const query = (await searchParams).query
 
-  const posts = [
-    {
-      _createdAt: new Date(),
-      views: 55,
-      author: {
-        _id: 1,
-        name: 'John Doe',
-      },
-      _id: 1,
-      description: 'This is a description',
-      title: 'We Robots',
-      category: 'Robots',
-      image:
-        'https://plus.unsplash.com/premium_photo-1681562502996-bcfad45d4def?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cm9ib3RzfGVufDB8fDB8fHww',
-    },
-  ]
+  const posts = await client.fetch(STARTUPS_QUERY)
+  console.log(JSON.stringify(posts, null, 2))
+
   return (
     <>
       <section className="pink_container">
@@ -46,7 +34,7 @@ export default async function Home({
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupCardType, index: number) => (
+            posts.map((post: StartupTypeCard, index: number) => (
               <StartupCard key={post?._id} post={post} />
             ))
           ) : (
