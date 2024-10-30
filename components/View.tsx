4cf8405b82b4aa325807d1692f5372f9a70d637a
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-10-30 15:00:16 +0530

    Views Functionality Done

--- components/View.tsx
+++ components/View.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Ping from './Ping'
 import { client } from '@/sanity/lib/client'
 import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries'
+import { writeClient } from '@/sanity/lib/write-client'
+import { unstable_after as after } from 'next/server'
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
@@ -10,7 +12,13 @@ const View = async ({ id }: { id: string }) => {
     })
     .fetch(STARTUP_VIEWS_QUERY, { id })
 
-  // TODO: MODIFY THE NUMBER OF VIEWS
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit(),
+  )
   return (
     <div className="view-container">
       <div className="absolute -top-2 -right-2">
